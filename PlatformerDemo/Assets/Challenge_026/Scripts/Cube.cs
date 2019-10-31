cc1225b99dcfa558a64844ee8b2cdce0e0d1cee9
Stonger44 <yakusoku44@gmail.com>
2019-10-30 19:24:40 -0700

    Challenge_026
    This is the commit where I passed a coding challenge as part of an interview and got accepted. Boom.

---
+++ PlatformerDemo/Assets/Challenge_026/Scripts/Cube.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Renderer _renderer = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = this.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_renderer.enabled == false)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            _renderer.enabled = false;
+
+            StartCoroutine(ShowCubeRoutine());
+        }
+    }
+
+    IEnumerator ShowCubeRoutine()
+    {
+        yield return new WaitForSeconds(5);
+
+        _renderer.enabled = true;
+    }
+}
